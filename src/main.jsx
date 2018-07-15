42196645cde013e90abab965057283fb1f5fcf2c
Szikszai Gusztáv <guszti5@hotmail.com>
2018-07-15 10:06:55 +0200

    Implement the counter component.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,79 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import ReactDOM from "react-dom";
 
-const StyledDiv = styled.div`
-  background: red;
+/* This is the styling for the base div. */
+const BaseDiv = styled.div`
+  background: ${props => props.background};
+  border-radius: 5px;
+  transition: 320ms;
+  display: flex;
+  padding: 20px;
+  margin: 20px;
 `
+
+/* This is the styling for the counter span. */
+const CounterSpan = styled.span`
+  font-family: sans;
+  font-size: 20px;
+  padding: 0 20px;
+`
+
+class Counter extends Component {
+  /* These are the property type definitons. */
+  props: {
+    onIncrement: () => void,
+    onDecrement: () => void,
+    counter: number
+  }
+
+  /* These are the default property values. */
+  static defaultProps = {
+    onIncrement: () => null,
+    onDecrement: () => null,
+    counter: 0
+  }
+
+  /* This is a computed property for the background color. */
+  get background () {
+    const { counter } = this.props
+
+    if (counter >= 10) {
+      return "lightgreen"
+    } else {
+      if (counter < 0) {
+        return "orangered"
+      } else {
+        return "#F2F2F2"
+      }
+    }
+  }
+
+  render () {
+    const { counter, onDecrement, onIncrement} = this.props
+
+    return <BaseDiv background={this.background}>
+      <button onClick={() => onDecrement()}>
+        Decrement
+      </button>
+
+      <CounterSpan>
+        { counter }
+      </CounterSpan>
+
+      <button onClick={() => onIncrement()}>
+        Increment
+      </button>
+    </BaseDiv>
+  }
+}
 
 class Main extends Component {
   render () {
-    return <StyledDiv>Hello World!</StyledDiv>
+    return <div>
+    <Counter counter={0}/>
+    <Counter counter={-1}/>
+    <Counter counter={11}/>
+    </div>
   }
 }
 
